24d9c96ee83978c8f8fc18894013a6a2da50678d
parth012001 <parthahir012001@gmail.com>
2025-05-07 17:45:19 -0700

    removed the navbar and the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter, useLocation } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import Home from './pages/Home';
@@ -12,34 +12,42 @@ import DigitizationService from './pages/DigitizationService';
 import Landing from './pages/Landing';
 //import Terms from './pages/Terms';
 
-function App() {
+function AppContent() {
   const [showStylistApplication, setShowStylistApplication] = useState(false);
+  const location = useLocation();
+  const isLandingPage = location.pathname === '/landing';
 
   const handleStylistApplicationOpen = () => {
     setShowStylistApplication(true);
   };
 
   return (
+    <div className="min-h-screen flex flex-col">
+      {!isLandingPage && <Navbar onApplyClick={handleStylistApplicationOpen} />}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/for-stylists" element={<ForStylists showApplicationForm={showStylistApplication} setShowApplicationForm={setShowStylistApplication} />} />
+          <Route path="/wardrobe-digitization" element={<DigitizationService />} />
+          <Route path="/landing" element={<Landing />} />
+        </Routes>
+      </main>
+      {!isLandingPage && <Footer />}
+      <AnimatePresence>
+        {showStylistApplication && (
+          <StylistApplicationForm onClose={() => setShowStylistApplication(false)} />
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar onApplyClick={handleStylistApplicationOpen} />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/for-stylists" element={<ForStylists showApplicationForm={showStylistApplication} setShowApplicationForm={setShowStylistApplication} />} />
-            <Route path="/wardrobe-digitization" element={<DigitizationService />} />
-            <Route path="/landing" element={<Landing />} />
-          </Routes>
-        </main>
-        <Footer />
-        <AnimatePresence>
-          {showStylistApplication && (
-            <StylistApplicationForm onClose={() => setShowStylistApplication(false)} />
-          )}
-        </AnimatePresence>
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
